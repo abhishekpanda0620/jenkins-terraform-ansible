189e82f08367e1ff1ab2f7f00e4276ba1b41e5a3
Abhishek Panda <abhishek.panda0620@gmail.com>
2025-10-06 17:07:26 +0530

    minor fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    use_lockfile = true
     bucket      = "jenkins-terraform-ansible-0610"
     key         = "terraform.tfstate"
     region      = "us-east-1"
+    use_lockfile = true
   }
 }
